9106cadc9a789e1ab984c0baf463098f61a5b08d
Hi, I'm Eric <61686801+Ericli2802@users.noreply.github.com>
2023-05-12 03:26:50 +0800

    test

---
+++ Source/Game/background.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+#include "config.h"
+
+namespace game_framework{
+
+	class BackGround {
+	public:
+		BackGround();
+		~BackGround();
+		void update(float dt);
+		void draw(float x, float y);
+
+	private:
+		// 圖片背景
+		CMovingBitmap background;
+		
+		// 陸地圖片
+		CMovingBitmap ground;
+
+		// 陸地圖片水平寬度
+		float groundMaxLandWidth;
+		float groundStartXaxis;
+
+	}
+};
